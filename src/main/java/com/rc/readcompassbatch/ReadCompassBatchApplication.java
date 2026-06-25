7137e8dfac5a94ab026f04be0cc8fbc071ab3032
gusdn6763 <gusdn6763@gmail.com>
2026-06-25 16:49:14 +0900

    feat: 스프링 프로젝트 기초 커밋

---
+++ src/main/java/com/rc/readcompassbatch/ReadCompassBatchApplication.java
@@ -0,0 +1,13 @@
+package com.rc.readcompassbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadCompassBatchApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ReadCompassBatchApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/rc/readcompassbatch/ReadCompassBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rc.readcompassbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadCompassBatchApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
